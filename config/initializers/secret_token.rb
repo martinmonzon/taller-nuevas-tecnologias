c7314e34ff43f69a685a530a8d149d230ede1a6b
martinmonzon <marmon17@hotmail.com>
2014-07-31 22:37:39 -0300

    Listo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prueba::Application.config.secret_key_base = 'eb268ff29a4994e7e3cc91302530194bfdd96f374239a3d8c9415142cc5c8a4949d9eb6451e8f7a06d03abce03d9649ed12705971438746e1095f027c112e420'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Prueba::Application.config.session_store :cookie_store, key: '_Prueba_session'
